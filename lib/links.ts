f65cf45f9deba0a5231216b28fc54fa570973ee9
Alex Young <alexyoung23j@gmail.com>
2024-04-03 21:14:29 -0400

    cleanup

--- lib/links.ts
+++ lib/links.ts
@@ -1,18 +1,6 @@
 export const navLinks = [
   {
-    route: "Home",
+    route: "Demo",
     path: "/",
-  },
-  {
-    route: "Features",
-    path: "/#features",
-  },
-  {
-    route: "Contact",
-    path: "/contact",
-  },
-  {
-    route: "Github",
-    path: "https://github.com/redpangilinan/next-shadcn-landing",
   },
 ]

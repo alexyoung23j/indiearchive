7dde7363ad26d581b3cc81094402213bde5282a5
Alex Young <alexyoung23j@gmail.com>
2025-02-15 17:32:05 -0500

    lol

--- components/pages/feature-cards.tsx
+++ components/pages/feature-cards.tsx
@@ -4,6 +4,7 @@ import { featureCards } from "@/config/contents"
 import { Card, CardDescription, CardTitle } from "@/components/ui/card"
 import { Icons } from "@/components/icons"
 
+
 export default function FeatureCards() {
   return (
     <section style={{ backgroundColor: '#f8fafc' }}>
